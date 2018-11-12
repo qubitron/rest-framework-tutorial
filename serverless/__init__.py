031f4666edae85ca48bfed0626d7afd992bc21e3
Carlton Gibson <carlton.gibson@noumenal.es>
2018-11-12 15:35:36 +0100

    Adjust application for functions.
    correct requirements

--- serverless/__init__.py
+++ serverless/__init__.py
@@ -1,24 +1,21 @@
 import logging
+import os
+import sys
+# Adjust PYTHONPATH for Django Application Folder
+sys.path.append(os.getcwd())  # noqa
+logging.info('sys.path: {}'.format(sys.path))  # noqa
 
 import azure.functions as func
+from wsgi_adapter import AzureWSGIHandler
+
+# Establish the base URL. Not super pretty...
+os.environ.setdefault('FUNCTIONS_MOUNT_POINT', 'api/serverless')  # noqa
+
+from tutorial.wsgi import application
+azure_application = AzureWSGIHandler(application)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    response: func.HttpResponse = azure_application(req)
+    return response
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,8 +1,15 @@
+import os
+
 from django.conf.urls import include, url
 
-urlpatterns = [
+base_urlpatterns = [
     url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 
+# Adjust URLs for base_url
+FUNCTIONS_MOUNT_POINT = os.getenv('FUNCTIONS_MOUNT_POINT')
+urlpatterns = [
+    url(FUNCTIONS_MOUNT_POINT + '/', include(base_urlpatterns)),
+]
